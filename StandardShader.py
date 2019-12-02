e04f063f6391b69786449944fdc6dd3ea52d1896
Caner Tasan <canertasan@sabanciuniv.edu>
2019-12-02 17:25:20 +0300

    End game

---
+++ StandardShader.py
@@ -0,0 +1,129 @@
+from typing import Iterable
+
+from OpenGL import GL as gl
+from OpenGL.GL import shaders
+from GameUtilities import *
+
+
+class StandardShader:
+    def __init__(self):
+        self.game_objects: GameObjectSet = set()
+
+        # Compile shaders
+        vertex_shader = shaders.compileShader("""#version 330
+        layout(location = 0) in vec3 vertex_position;
+        layout(location = 1) in vec2 vertex_texture_position;
+        layout(location = 2) in vec3 vertex_normal;
+        uniform mat4 projection;
+        uniform mat4 view;
+        uniform mat4 transformation;
+
+        out vec2 texture_position;
+        out vec3 normal;
+        out vec3 world_position;
+
+        void main()
+        {
+            //  Because the transformation matrix is 4x4, we have to construct a vec4 from position, so we an multiply them
+            vec4 pos = vec4(vertex_position, 1.0);
+            gl_Position = projection * view * transformation * pos;
+
+            texture_position = vertex_texture_position;
+
+            vec4 transformed_normal = transpose(inverse(transformation)) * vec4(vertex_normal, 0.0);
+            normal = normalize(transformed_normal.xyz);
+
+            world_position = (transformation * pos).xyz;
+        }
+        """, gl.GL_VERTEX_SHADER)
+
+        fragment_shader = shaders.compileShader("""#version 420
+layout(binding = 0) uniform sampler2D ambient_color_sampler; 
+layout(binding = 1) uniform sampler2D diffuse_color_sampler; 
+layout(binding = 2) uniform sampler2D specular_color_sampler;
+uniform float shininess;
+in vec2 texture_position;
+in vec3 normal;
+in vec3 world_position;
+uniform vec3 camera_position;
+uniform vec3 ambient_color;
+uniform vec3 diffuse_color;
+uniform vec3 specular_color;
+uniform vec3 light_position;
+out vec4 fragment_color;
+
+void main()
+{
+	
+	
+	vec3 ambient = ambient_color * texture(diffuse_color_sampler, texture_position).rgb;
+
+	vec3 norm = normalize(normal);
+	vec3 lightDir = normalize(light_position - world_position);
+	float diff = max(dot(norm,lightDir), 0.0);
+	vec3 diffuse = diffuse_color * diff * texture(diffuse_color_sampler, texture_position).rgb;
+
+
+	vec3 viewDir = normalize( camera_position - world_position);
+	vec3 reflectDir = reflect(-lightDir, norm);
+	float spec = pow(max(dot(viewDir,reflectDir), 0.0), shininess);
+	vec3 specular = specular_color * spec * texture(specular_color_sampler,texture_position).rgb;
+	
+	
+	vec3 color = ambient + diffuse + specular;
+
+	fragment_color = vec4(color, 1.0);
+}
+        """, gl.GL_FRAGMENT_SHADER)
+
+        # Compile the program
+        self.id = shaders.compileProgram(vertex_shader, fragment_shader)
+
+        # Get the locations
+        gl.glUseProgram(self.id)
+        self.transformation_location = gl.glGetUniformLocation(self.id, "transformation")
+        self.view_location = gl.glGetUniformLocation(self.id, "view")
+        self.projection_location = gl.glGetUniformLocation(self.id, "projection")
+        self.light_position_location = gl.glGetUniformLocation(self.id, "light_position")
+        self.camera_position_location = gl.glGetUniformLocation(self.id, "camera_position")
+        self.ambient_color_location = gl.glGetUniformLocation(self.id, "ambient_color")
+        self.diffuse_color_location = gl.glGetUniformLocation(self.id, "diffuse_color")
+        self.specular_color_location = gl.glGetUniformLocation(self.id, "specular_color")
+        self.shininess_location = gl.glGetUniformLocation(self.id, "shininess")
+        gl.glUseProgram(0)
+
+    def draw(self, projection, view, camera_position, light_position, game_objects: Iterable[GameObject] = None):
+        if game_objects is None:
+            game_objects = self.game_objects
+
+        gl.glUseProgram(self.id)
+
+        gl.glUniformMatrix4fv(self.projection_location, 1, False, glm.value_ptr(projection))
+        gl.glUniformMatrix4fv(self.view_location, 1, False, glm.value_ptr(view))
+        gl.glUniform3fv(self.camera_position_location, 1, glm.value_ptr(camera_position))
+        gl.glUniform3fv(self.light_position_location, 1, glm.value_ptr(light_position))
+
+        for game_object in game_objects:
+            # Set the vao
+            gl.glBindVertexArray(game_object.obj_data.vao)
+
+            # Set the transform uniform to self.transform
+            gl.glUniformMatrix4fv(self.transformation_location, 1, False, glm.value_ptr(game_object.transform.get_matrix()))
+
+            for mesh in game_object.obj_data.meshes:
+                # Set material uniforms
+                gl.glUniform3fv(self.ambient_color_location, 1, glm.value_ptr(mesh.material.Ka))
+                gl.glUniform3fv(self.diffuse_color_location, 1, glm.value_ptr(mesh.material.Kd))
+                gl.glUniform3fv(self.specular_color_location, 1, glm.value_ptr(mesh.material.Ks))
+                gl.glUniform1fv(self.shininess_location, 1, mesh.material.Ns)
+
+                # Set material textures
+                gl.glActiveTexture(gl.GL_TEXTURE0)
+                gl.glBindTexture(gl.GL_TEXTURE_2D, mesh.material.map_Ka)
+                gl.glActiveTexture(gl.GL_TEXTURE1)
+                gl.glBindTexture(gl.GL_TEXTURE_2D, mesh.material.map_Kd)
+
+                # Draw the mesh with an element buffer.
+                gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, mesh.element_array_buffer)
+                # When the last parameter in 'None', the buffer bound to the GL_ELEMENT_ARRAY_BUFFER will be used.
+                gl.glDrawElements(gl.GL_TRIANGLES, mesh.element_count, gl.GL_UNSIGNED_INT, None)
